bed1e3cb49ac6688c43c0c87d35278a205eb08b0
ZachLuttrell <zacharywluttrell@email.arizona.edu>
2015-10-21 22:25:29 -0700

    Updated PaintObjects

--- src/PaintObjects/Line.java
+++ src/PaintObjects/Line.java
@@ -9,12 +9,15 @@ public class Line extends PaintObject {
 	private Line2D.Double line;
 	
 	public Line(Graphics2D graphic, int startX, int startY, int endX, int endY, Color color){
+		//Initialize fields
 		setGraphics(graphic);
 		setStartX(startX);
 		setStartY(startY);
 		setEndX(endX);
 		setEndY(endY);
 		setColor(color);
+		
+		//Initialize shape
 		line = new Line2D.Double(startX, startY, endX, endY);
 	}
 	
